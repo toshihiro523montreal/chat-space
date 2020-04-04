f36484c18c3ec7265ef6f427871f57cd6fdfae2b
toshihiro523montreal <62530973+toshihiro523montreal@users.noreply.github.com>
2020-04-04 13:34:47 +0900

    [WIP]Auto-Updating-branch
    自動更新実装２

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,3 @@
-
 $(function(){ 
   function buildHTML(message){
   if ( message.image ) {
@@ -45,10 +44,6 @@ $(function(){
   var formData = new FormData(this);
   $('.submit-btn').removeAttr('data-disable-with');
   var url = $(this).attr('action')
-
-  var reloadMessages = function() {
-    var last_message_id = $('.message:last').data("message-id");
-    
     $.ajax({
       url: url,
       type: "POST",
@@ -66,6 +61,30 @@ $(function(){
         .fail(function() {
           alert('メッセージを送信できません');
         });
-      })
-    });
 });
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+})
