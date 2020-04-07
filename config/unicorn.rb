f34119dfc253bc111ba0f8284afbc76d101a3104
toshihiro523montreal <62530973+toshihiro523montreal@users.noreply.github.com>
2020-04-07 19:34:40 +0900

    Updating unicorn.rb2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -53,34 +53,3 @@ end
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
-
-
-
-
-
-
-
-app_path = File.expand_path('../../', __FILE__)
-
-worker_processes 1
-
-working_directory app_path
-pid "#{app_path}/tmp/pids/unicorn.pid"
-listen "#{app_path}/tmp/sockets/unicorn.sock"
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-stdout_path "#{app_path}/log/unicorn.stdout.log"
-
- ↓↓↓↓↓↓↓ 以下のように変更 ↓↓↓↓↓↓
-
-# ../が一つ増えている
-app_path = File.expand_path('../../../', __FILE__)
-
-worker_processes 1
-# currentを指定
-working_directory "#{app_path}/current"
-
-# それぞれ、sharedの中を参照するよう変更
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
